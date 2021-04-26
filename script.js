1ab91a44374ab69dfb29cbf507c33b53a84c386e
Eric <77696755+EricAkaRuiz@users.noreply.github.com>
2021-04-25 19:54:38 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-Button');
+const todoList = document.querySelector('.todo-List');
+
+//Event listeners
+todoButton.addEventListener('click', addTodo);
+//Functions
+
+function addTodo(event){
+    console.log('hello');
+}
