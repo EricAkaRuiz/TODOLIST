55db0350f89b9e4212ebfc98c87f23ac08ffcbcd
Eric <77696755+EricAkaRuiz@users.noreply.github.com>
2021-04-25 20:55:59 -0500

    js error

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 //Selectors
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-Button');
-const todoList = document.querySelector('.todo-List');
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-Button");
+const todoList = document.querySelector(".todo-List");
 
 //Event listeners
+ 
 todoButton.addEventListener("click", addTodo);
-//Functions
 
-function addTodo(event){
+ //Functions
+
+function addTodo(event) {
+    //Prevent Form From submitting
     event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create LI
+    const newtodo = document.createElement('li');
+    newtodo.innerText = 'hey';
+    newtodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check MARK Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //CHECK TRASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    //PPEND TO LIST
+    todoList.appendChild(todoDiv);
 }
